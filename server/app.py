2248570d59d4042a47051219da6ce21f1c445abb
toel-kamanda34 <toelokemwa@gmail.com>
2024-10-17 00:11:05 +0300

    Completed passwords lab

--- server/app.py
+++ server/app.py
@@ -9,9 +9,9 @@ from models import User
 class ClearSession(Resource):
 
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session.clear()
+        #session['page_views'] = None
+        #session['user_id'] = None
 
         return {}, 204
 
@@ -25,19 +25,44 @@ class Signup(Resource):
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return user.to_dict(), 200
+        
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json['username']
+        password = json['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.clear()
+        return {'message': 'Successfully logged out'},200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
